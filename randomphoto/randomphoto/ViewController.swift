c82a112472d2e54feb4023493e3d51acf615f6d2
Nicole S <nicolec27@mac.mynetworksettings.com>
2025-01-29 23:51:40 -0500

    added files

---
+++ randomphoto/randomphoto/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  randomphoto
+//
+//  Created by Nicole S on 1/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemCyan,
+        .systemMint,
+        .systemPurple,
+        .systemIndigo,
+        .systemTeal
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 300)
+        imageView.center = view.center
+        
+        view.addSubview(button)
+       
+        getRandomPhoto()
+        
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        // Do any additional setup after loading the view.
+    }
+    
+    @objc func didTapButton() {
+        getRandomPhoto()
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(x: 30, y: view.frame.size.height-150-view.safeAreaInsets.bottom, width: view.frame.size.width-60, height: 55)
+    }
+    
+    func getRandomPhoto() {
+        let urlString = "https://picsum.photos/200"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+        
+    }
+
+
+}
+
---
+++ randomphoto/randomphotoTests/randomphotoTests.swift
@@ -0,0 +1,17 @@
+//
+//  randomphotoTests.swift
+//  randomphotoTests
+//
+//  Created by Nicole S on 1/29/25.
+//
+
+import Testing
+@testable import randomphoto
+
+struct randomphotoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
